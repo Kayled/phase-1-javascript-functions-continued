ebb37df854ddcc57e657d7742d15edd01979bbdf
+ <+>
2021-09-22 11:58:58 -0700

    commit tree 827aca56626234f5c7e4d8111abe152b1f91e6d8

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function (activity = "go to the office") {
+        return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(deer = "*"){
+    return function (emphatic = "a hard worker") {
+            // const emphatic = `${deer}${emphatic}`;
+            return `You are ${deer}${emphatic}${deer}!`;
+        };
+    };
+
+
+    // function wrapAdjective(bear = "||"){
+    //     return function (emphasis = "a dedicated programer") {
+    //             // const emphatic = `${deer}${emphatic}`;
+    //             return `You are ${bear}${emphasis}${bear}!`;
+    //         };
+    //     };
+
+
+
+
+
+// function wrapAdjective(emphasis = "a dedicated programer"){
+//     return function(emphatic){
+//         return function (moon = "||"){
+//             const emphasis = `${moon}${emphasis}${moon}`;
+//             return `You are ${moon}${emphasis}${moon}!`;
+//         };
+//     };
+// }
